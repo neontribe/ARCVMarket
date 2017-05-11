25a486fe2bc71b2f584de8b0413f445b2dda3dae
Steph Adams <steph@neontribe.co.uk>
2017-05-11 16:06:23 +0100

    Add placeholder text for market stall name, fix typo in package.json

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,9 @@
 
         <div class="content">
 
-            <h1>Hello, Steph.</h1>
+            <h1>Hello, Market Stall Name.</h1>
 
-            <p>You have added 30 valid vouchers.</p>
+            <p>You have added 31 valid vouchers.</p>
 
             <router-link v-bind:to="'/tap'" class="link"><button>Add vouchers</button></router-link>
             <router-link v-bind:to="'/send'" class="link"><button>Request payment</button></router-link>
