d6945fa342c53c233b7dbc6b10143f1688263361
Steph Adams <steph@neontribe.co.uk>
2017-05-19 15:43:25 +0100

    Voucher list markup

--- src/pages/Payment.vue
+++ src/pages/Payment.vue
@@ -1,14 +1,61 @@
 <template>
     <div>
-        <main class="container" id="payment">
+      <main class="container fullwidth" id="payment">
 
-            <div class="content">
+          <div class="content fullwidth">
+              <h1>My Voucher List</h1>
 
-                <h1>My Voucher List</h1>
+              <p>You can request payment for <strong>3</strong> vouchers.</p>
 
-            </div>
+              <div class="voucher-list">
 
-        </main>
+                  <!-- Tab header -->
+                  <div class="tab thead">
+                      <label for="tab-one">
+                          <div class="row">
+                              <div>Voucher code</div>
+                              <div>Date added</div>
+                          </div>
+                      </label>
+                  </div>
+
+                  <!-- Tab row -->
+                  <div class="tab row">
+                      <label for="tab-two">
+                          <div class="row">
+                              <div>RVP12345565</div>
+                              <div>15/2/17</div>
+                          </div>
+                      </label>
+                  </div>
+
+                  <!-- Tab row -->
+                  <div class="tab row">
+                      <label for="tab-two">
+                          <div class="row">
+                              <div>RVP12345569</div>
+                              <div>1/3/17</div>
+                          </div>
+                      </label>
+                  </div>
+
+                  <!-- Tab row -->
+                  <div class="tab row">
+                      <label for="tab-two">
+                          <div class="row">
+                              <div>RVP12345561</div>
+                              <div>21/3/17</div>
+                          </div>
+                      </label>
+                  </div>
+
+              </div>
+
+              <button class="smaller">Get payment</button>
+
+          </div>
+
+      </main>
     </div>
 </template>
 
