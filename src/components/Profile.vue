09e763b70cc56f43c339f302a8d8017085caca4b
Steph Adams <steph@neontribe.co.uk>
2017-05-25 19:33:09 +0100

    Add ripple effect to buttons

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,7 +2,7 @@
 
     <div class="profile-bar">
         <div v-if="recVouchers[0] && recVouchers[0].length > 0"><strong>{{ recVouchers[0].length }}</strong> vouchers waiting</div>
-        <div>Logged in as <router-link v-bind:to="'/account'">P &amp; J Millward</router-link></div>
+        <div>Logged in as <router-link v-bind:to="'/user'">P &amp; J Millward</router-link></div>
     </div>
 
 </template>
