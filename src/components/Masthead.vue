f493d8cbe820cb6c729d20277206e42be9a9fa40
Steph Adams <steph@neontribe.co.uk>
2017-05-26 11:03:15 +0100

    Add functions to display toolbar and full header content

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -3,11 +3,11 @@
     <div class="wrapper">
 
         <header role="header">
-            <div class="logo">
+            <div class="logo" v-bind:class="{ login : $route.path==='/login' || $route.path==='/user' }">
                 <router-link v-bind:to="'/'" class="link"><img src="../assets/logo.png"></router-link>
             </div>
 
-            <nav role="navigation">
+            <nav role="navigation" v-if="headerFull">
                 <ul>
                     <li><router-link v-bind:to="'/'"><i class="fa fa-keyboard-o" aria-hidden="true"></i><br />Add Voucher</router-link></li>
                     <li><router-link v-bind:to="'/payment'"><i class="fa fa-rocket" aria-hidden="true"></i><br />Queued<br />Vouchers</router-link></li>
@@ -17,9 +17,8 @@
 
         </header>
 
-        <profile></profile>
-
-        <transition name="fade"><toolbar></toolbar></transition>
+        <profile v-if="headerFull"></profile>
+        <toolbar v-if="toolbarShow"></toolbar>
 
     </div>
 
@@ -33,6 +32,23 @@ export default {
     components: {
         Profile,
         Toolbar
+    },
+    computed : {
+        headerFull : function() {
+            var paths = [
+              "/login",
+              "/user"
+            ];
+            return (paths.indexOf(this.$route.path) === -1);
+        },
+        toolbarShow : function() {
+            var paths = [
+              "/",
+              "/scan",
+              "/upload"
+            ];
+            return !(paths.indexOf(this.$route.path) === -1);
+        }
     }
 }
 </script>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,6 +1,6 @@
 <template id="toolbar">
 
-    <div class="toolbar" v-if="($route.path==='/' || $route.path==='/scan' || $route.path==='/upload') ? true : false">
+    <div class="toolbar">
         <router-link v-bind:to="'/'"><i class="fa fa-keyboard-o" aria-hidden="true"></i></router-link>
         <router-link v-bind:to="'/scan'"><i class="fa fa-camera" aria-hidden="true"></i></router-link>
         <router-link v-bind:to="'/upload'"><i class="fa fa-upload" aria-hidden="true"></i></router-link>
