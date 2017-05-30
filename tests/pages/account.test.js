f32a239e4430a08b3d258992002bb851e82ab9af
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-05-30 09:57:05 +0100

    account page tests added

---
+++ tests/pages/account.test.js
@@ -0,0 +1,70 @@
+import { expect } from 'chai';
+import { Selector } from 'testcafe';
+
+const el = Selector(selector => document.querySelector(selector));
+
+const url = 'http://localhost:8081';
+
+fixture `Account Page`
+    .page(url);
+
+test('Account page exists', async t => {
+	await t
+	    .typeText('#userName', 'email@example.com')
+	    .typeText('#userPassword', 'secretpass')
+	    .pressKey('enter')
+	;
+	const accountNavButton = await el('a[href*="/account"');
+
+	await t
+		.click(accountNavButton)
+	;
+	const pagePath = await t.eval(() => window.location);
+	expect(pagePath.pathname).eql('/account');
+});
+
+test('Header text is correct', async t => {
+	await t
+	    .typeText('#userName', 'email@example.com')
+	    .typeText('#userPassword', 'secretpass')
+	    .pressKey('enter')
+	;
+	const accountNavButton = await el('a[href*="/account"');
+
+	await t
+		.click(accountNavButton)
+	;
+    const header = await el('main#account .content h1').innerText;
+    expect(header).to.equal('Requested Payments');
+});
+
+test('Requested payments accordion exists', async t => {
+	await t
+		.typeText('#userName', 'email@example.com')
+	    .typeText('#userPassword', 'secretpass')
+	    .pressKey('enter')
+	;
+	const accountNavButton = await el('a[href*="/account"');
+
+	await t
+		.click(accountNavButton)
+	;
+	const paymentsAccordion = await el('main#account .content div.accordion').exists;
+	expect(paymentsAccordion).to.be.ok;
+});
+
+test('Logout and download buttons exist', async t => {
+	await t
+		.typeText('#userName', 'email@example.com')
+	    .typeText('#userPassword', 'secretpass')
+	    .pressKey('enter')
+	;
+	const accountNavButton = await el('a[href*="/account"');
+
+	await t
+		.click(accountNavButton)
+	;
+	const logoutButton = await el('.two-buttons .left');
+	const downloadButton = await el('.two-buttons .right');
+	expect(logoutButton && downloadButton).to.be.ok;
+});
