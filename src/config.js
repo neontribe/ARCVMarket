8fe0d8c11019c2464e1774046de4146ec8ffb8c1
Charles Strange <charles@neontribe.co.uk>
2017-06-08 09:48:56 +0100

    Bump v1.0.0

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // --- Defaults ---
 var env = 'production',
     apiBase = 'https://arcvservice-prealpha.neontribe.org/api',
-    appVersion = 'v0.2.0';
+    appVersion = 'v1.0.0';
 
 // --- Env Specific ---
 if (location.hostname.match(/localhost|(\.(dev|app))$/)) {
