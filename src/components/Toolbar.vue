2583e378ba14013d0144fb30934d930924bc9c4d
Steph Adams <steph@neontribe.co.uk>
2017-06-12 12:27:47 +0100

    Change camera to barcode icon

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -6,7 +6,7 @@
         </transition>
         <div class="input-icons">
             <router-link v-bind:to="'/'"><i class="fa fa-keyboard-o" aria-hidden="true"></i></router-link>
-            <router-link v-bind:to="'/scan'"><i class="fa fa-camera" aria-hidden="true"></i></router-link>
+            <router-link v-bind:to="'/scan'"><i class="fa fa-barcode" aria-hidden="true"></i></router-link>
             <router-link v-bind:to="'/upload'"><i class="fa fa-upload" aria-hidden="true"></i></router-link>
         </div>
     </div>
