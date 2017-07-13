c47fa80422b30fb26aeaa6d437329b8453e496e7
Steph Adams <steph@neontribe.co.uk>
2017-07-13 19:56:21 +0100

    Fix logic

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -7,7 +7,7 @@
 
                 <transition name="fade"><div v-if="errorMessage" v-bind:class="[goodFeedback ? 'good message' : 'message' ]">{{ errorMessage }}</div></transition>
 
-                <div v-if="voucherPayments.length == 0">
+                <div v-if="voucherPayments.length > 0">
 
                     <div>
                         <p>Click the <span class="list-icon"><i class="fa fa-list" aria-hidden="true"></i></span> icon below to view a payment history record in more detail.</p>
