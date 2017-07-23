afbc2cf41f9e379469b422a09b32eca3de384823
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-07-23 19:27:41 +0100

    Wire up ChangePassword form to POST to endpoint user/lost_password/reset

--- src/pages/ChangePassword.vue
+++ src/pages/ChangePassword.vue
@@ -10,12 +10,12 @@
                 <p>Please enter your new password below.</p>
 
                 <div>
-                    <form id="resetPassword">
+                    <form id="resetPassword" v-on:submit.prevent>
                         <label for="resetPassword">Reset password</label>
                         <input type="text" id="resetPassword" required>
                         <label for="confirmPassword">Confirm password</label>
-                        <input type="text" id="confirmPassword" required>
-                        <button type="submit" value="Request new password">Reset password</button>
+                        <input type="text" id="confirmPassword" v-model="pwd" required>
+                        <button type="submit" value="Request new password" v-on:click="onRequestChangePassword">Reset password</button>
                     </form>
                 </div>
 
@@ -30,6 +30,24 @@
     import Config from '../config.js';
     import Store from '../store.js';
     export default {
-        name: 'changePassword'
+        name: 'changePassword',
+        data: function () {
+            return {
+                netMgr : Store.netMgr,
+                pwd : ""
+            }
+        },
+        methods: {
+            onRequestChangePassword: function () {
+                return this.netMgr.apiPost('user/lost_password/reset', {'password' : this.pwd},
+                      function (response) {
+                          if (success) {success(response)}
+                      },
+                      function (error) {
+                          if (failure) {failure(error)}
+                      }
+                )
+            }
+        }
     }
 </script>
