bca600e91a4c0fa87441f76ab08ab746ffe84c56
Charles Strange <charles@neontribe.co.uk>
2020-12-08 19:00:57 +0000

    Add eslint/prettier for vue

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        sourceType: "module",
+        parser: "babel-eslint",
+    },
+    env: {
+        browser: true,
+    },
+    extends: ["prettier", "prettier/standard", "plugin:vue/recommended"],
+    plugins: ["vue", "prettier"],
+    rules: {
+        "prettier/prettier": "error",
+        curly: ["error", "all"],
+        "no-confusing-arrow": ["error", { allowParens: false }],
+        "vue/html-self-closing": ["error", { html: { void: "any" } }],
+    },
+};
