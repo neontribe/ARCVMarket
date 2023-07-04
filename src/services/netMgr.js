441f741227c46986404a62f12118f4204afb2e14
Tobias Batch <tobias@neontribe.co.uk>
2023-07-04 14:33:39 +0100

    fix: don't use client side logging in dev envs

--- src/services/netMgr.js
+++ src/services/netMgr.js
@@ -210,6 +210,10 @@ NetMgr.axiosInstance.interceptors.response.use(
 );
 
 function stash(response) {
+    if (Config.env !== "production" && Config.env !== "test") {
+        // don't bother logging in we aren't live
+        return;
+    }
     response.created = Date.now();
     response.trader = Store.trader.id;
     const data = localStorage.getItem("arcLogs");
